45abcba07026ed1ab254b90e72c391a58fef0b0d
Katrina Fuller <trinafuller@att.net>
2019-08-29 15:05:38 -0600

    all set up like previous example

--- app/Controllers/MyTunesController.js
+++ app/Controllers/MyTunesController.js
@@ -3,14 +3,29 @@ import MyTunesService from "../Services/MyTunesService.js";
 //Private
 let _myTunesService = new MyTunesService()
 
+function _draw() {
+    let elem = document.getElementById('songs')
+    let songs = _myTunesService.Songs
+    let template = '<ul'
+    songs.forEach(s => {
+        template += s.Template
+    })
+    elem.innerHTML = template + '</ul>'
+}
 
 
 //Public
 export default class MyTunesController {
     constructor() {
         //NOTE Register all subscribers
-
+        _myTunesService.addSubscriber("songs", _draw)
 
         //NOTE Retrieve data
+        _myTunesService.getMusicByQuery('ccr')
+    }
+
+    search(e) {
+        e.preventDefault()
+        _myTunesService.getMusicByQuery(e.target.query.value)
     }
 }
